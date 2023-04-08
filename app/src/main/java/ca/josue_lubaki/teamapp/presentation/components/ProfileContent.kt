a834bb3dda5d3b71c776cfdab90ff29c23cdb9ca
Josue Lubaki <josue.lubaki@alithya.com>
2023-04-08 09:05:11 -0400

    updated README file & code cleaning

--- app/src/main/java/ca/josue_lubaki/teamapp/presentation/components/ProfileContent.kt
+++ app/src/main/java/ca/josue_lubaki/teamapp/presentation/components/ProfileContent.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ca.josue_lubaki.teamapp.data.db.local.Profession
@@ -27,7 +28,8 @@ import ca.josue_lubaki.teamapp.ui.theme.dimensions
 fun ProfileContent(
     fullName : String,
     profession: Profession,
-    horizontalAlignment : Alignment.Horizontal = Alignment.Start
+    horizontalAlignment : Alignment.Horizontal = Alignment.Start,
+    textStyle: TextStyle = MaterialTheme.typography.headlineSmall
 ) {
     Column(
         modifier = Modifier.padding(MaterialTheme.dimensions.small),
@@ -35,7 +37,7 @@ fun ProfileContent(
     ) {
         Text(
             text = fullName,
-            style = MaterialTheme.typography.headlineSmall
+            style = textStyle
         )
 
         CompositionLocalProvider(
